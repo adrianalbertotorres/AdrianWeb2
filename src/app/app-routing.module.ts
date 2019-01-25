ca1af93919182ef0825984974bf58704ac55da46
Adriantorres <adrianalbertotorres@gmail.com>
2019-01-25 13:31:42 -0300

    Inicio

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ContactoComponent } from './contacto/contacto.component';
+import { InicioComponent } from './inicio/inicio.component';
+import { PaginasWebComponent } from './paginas-web/paginas-web.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+{path:'contacto',component:ContactoComponent},
+{path:'inicio',component:InicioComponent},
+{path:'paginas-web',component:PaginasWebComponent},
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,36 @@
+import { RouterModule, Routes } from '@angular/router';
+
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { InicioComponent } from './inicio/inicio.component';
+import { PaginasWebComponent } from './paginas-web/paginas-web.component';
+import { ContactoComponent } from './contacto/contacto.component';
 
+
+const routes: Routes = [
+  { path: 'inicio', component:InicioComponent },
+ 
+  
+
+  { path: '', component:InicioComponent, pathMatch: 'full' },
+  { path: '**', redirectTo: '/', pathMatch: 'full' },
+];
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    InicioComponent,
+    PaginasWebComponent,
+    ContactoComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
